051a01526b42f4670c2bd28b67b96ac849990a43
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2018-08-15 23:39:05 +0300

    Add Prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const StylelintPlugin = require('stylelint-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin')
 
 module.exports = {
   /*
@@ -9,11 +9,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
   ** Customize the progress bar color
@@ -26,14 +24,14 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        });
+          exclude: /(node_modules)/,
+        })
 
         // TODO: Проверка стилей при сборке
         /*config.plugins.push(new StylelintPlugin({
@@ -43,7 +41,6 @@ module.exports = {
           ],
         }))*/
       }
-    }
-  }
+    },
+  },
 }
-
